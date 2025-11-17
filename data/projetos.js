99b16f6ee154a512fabc1e0027a46ae11ca7edf0
Ana Gaspar <anajulia3907@gmail.com>
2025-11-17 10:05:36 -0300

    feat: Ajustado os dados exibidos em projetos

--- data/projetos.js
+++ data/projetos.js
@@ -6,7 +6,7 @@ const projetos = [
         imagem: "/img/spoiler-projetos/spoiler-api1.png",
         problema: "O projeto foi proposto pelo Professor Fernando Masanori, o problema citado por ele foi que os moradores de São José dos Campos tinham dificuldade para encontrar informações claras sobre o trabalho dos vereadores. As informações até existiam, mas estavam espalhadas ou pouco acessíveis, o que tornava difícil acompanhar quem realmente estava atuando de forma efetiva na cidade.",
         solucao: "Criamos uma plataforma web que disponibiliza informações sobre o desempenho dos vereadores da cidade de São José dos Campos durante o atual mandato. O objetivo é oferecer aos eleitores dados claros e acessíveis que ajudem a tomar decisões informadas nas eleições municipais.",
-        contribuicao: "No início, fiquei responsável por montar os wireframes no Figma, usados pra apresentar o protótipo ao cliente. Depois, desenvolvi a estrutura em HTML e fiz a estilização em CSS das telas de gráficos e da área de comentários. Também programei a barra de pesquisa em JavaScript, que permite filtrar informações dentro da página.",
+        contribuicao: "No início, fiquei responsável por montar os wireframes no Figma, usados pra apresentar o protótipo ao cliente. Depois, desenvolvi parte da estrutura em HTML e fiz a estilização em CSS das telas de gráficos e da área de comentários. Também programei a barra de pesquisa, utilizando JavaScript, que permite filtrar informações dentro da página.",
         papel: "Dev Team",
         softSkills: ["Comunicação", "Trabalho em equipe", "Pensamento crítico"],
         hardSkills: ["Criação de wireframes", "HTML e CSS", "JavaScript"],
@@ -21,7 +21,7 @@ const projetos = [
         imagem: "/img/spoiler-projetos/spoiler-api2.png",
         problema: "A Helpnei precisava de um jeito melhor de mostrar para as empresas parceiras o impacto e os resultados obtidos dentro da plataforma. Até então, esses dados eram pouco organizados e difíceis de visualizar.",
         solucao: "Desenvolvemos um dashboard de indicadores para monitorar o impacto da plataforma e acompanhar o crescimento das empresas cadastradas no Helpnei, exibindo dados estratégicos para patrocinadores e investidores. E um sistema de captação de usuários interessados em patrocínio.",
-        contribuicao: "Fiquei responsável por montar a estrutura das páginas em HTML e fazer toda a parte visual com CSS. Também desenvolvi um mapa interativo, que mostra a distribuição geográfica dos usuários.",
+        contribuicao: "Fui uma das responsáveis por montar a estrutura das páginas em HTML e fazer a parte visual com CSS. Também desenvolvi um mapa interativo, que mostra a distribuição geográfica dos usuários.",
         papel: "Dev Team",
         softSkills: ["Colaboração", "Resolução de problemas", "Organização e planejamento"],
         hardSkills: ["HTML e CSS", "Noções de UX design", "Visualização de dados"],
@@ -48,12 +48,12 @@ const projetos = [
         id: 4,
         titulo: "Telegram Bibliobot",
         imagem: "/img/spoiler-projetos/spoiler-chatbot.png",
-        solucao: "Desenvolvimento de um chatbot para Telegram para ajudar usuários a encontrar livros de forma rápida e fácil. É possível pesquisar por título, autor ou gênero, receber recomendações personalizadas e descobrir novos títulos de interesse em pouquíssimo tempo ",
+        solucao: "Desenvolvimento de um chatbot para Telegram para ajudar usuários a encontrar livros de forma rápida e fácil. É possível pesquisar por título, autor ou gênero, receber recomendações personalizadas e descobrir novos títulos de interesse em pouquíssimo tempo.",
         contribuicao: "Atuei na personalização do modelo de linguagem e na integração do chatbot com o Telegram, garantindo que as respostas fossem adequadas ao contexto e que o funcionamento dentro da plataforma fosse fluido.",
         hardSkills: ["LLMs", "Ollama", "IA", "Google Colab"],
         tecnologias: ["Python", "Ollama", "SQLite", "Fast MCP", "Fast API"],
         github: "https://github.com/anajgaspar/chatbot-IHC",
-        deploy: ""
+        youtube: ""
     }
 ];
 
