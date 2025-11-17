28da7f0122a789c4a3539c8adc4efd2389eff785
Ana Gaspar <anajulia3907@gmail.com>
2025-11-17 13:59:49 -0300

    feat: Ajustado os dados exibidos em projetos

--- data/projetos.js
+++ data/projetos.js
@@ -4,12 +4,38 @@ const projetos = [
         titulo: "Desempenho de Vereadores",
         semestre: "1° Semestre - 2024-2",
         imagem: "/img/spoiler-projetos/spoiler-api1.png",
-        problema: "O projeto foi proposto pelo Professor Fernando Masanori, o problema citado por ele foi que os moradores de São José dos Campos tinham dificuldade para encontrar informações claras sobre o trabalho dos vereadores. As informações até existiam, mas estavam espalhadas ou pouco acessíveis, o que tornava difícil acompanhar quem realmente estava atuando de forma efetiva na cidade.",
+        problema: "O projeto foi proposto pelo professor Fernando Masanori, o problema citado por ele foi que os moradores de São José dos Campos tinham dificuldade para encontrar informações claras sobre o trabalho dos vereadores. As informações até existiam, mas estavam espalhadas ou pouco acessíveis, o que tornava difícil acompanhar quem realmente estava atuando de forma efetiva na cidade.",
         solucao: "Criamos uma plataforma web que disponibiliza informações sobre o desempenho dos vereadores da cidade de São José dos Campos durante o atual mandato. O objetivo é oferecer aos eleitores dados claros e acessíveis que ajudem a tomar decisões informadas nas eleições municipais.",
         contribuicao: "No início, fiquei responsável por montar os wireframes no Figma, usados pra apresentar o protótipo ao cliente. Depois, desenvolvi parte da estrutura em HTML e fiz a estilização em CSS das telas de gráficos e da área de comentários. Também programei a barra de pesquisa, utilizando JavaScript, que permite filtrar informações dentro da página.",
         papel: "Dev Team",
-        softSkills: ["Comunicação", "Trabalho em equipe", "Pensamento crítico"],
-        hardSkills: ["Criação de wireframes", "HTML e CSS", "JavaScript"],
+        softSkills: [
+            {
+                titulo: "Comunicação", 
+                descricao: "Aprimorei a comunicação com a equipe, especialmente durante as fases de alinhamento de ideias e apresentação dos protótipos."
+            }, 
+            {
+                titulo:"Trabalho em equipe", 
+                descricao: "Colaborei diretamente com o restante da equipe de desenvolvimento, tirando dúvidas e debatendo opiniões."
+            }, 
+            {
+                titulo: "Pensamento crítico",
+                descricao: "Analisei limitações do projeto e propus melhorias visuais e funcionais com base na experiência do usuário."
+            }
+        ],
+        hardSkills: [
+            {
+                titulo: "Criação de wireframes", 
+                descricao: "Desenvolvi wireframes funcionais no Figma, transformando as ideias do cliente em telas claras e organizadas."
+            }, 
+            {
+                titulo: "HTML e CSS", 
+                descricao: "Trabalhei na construção e estilização das páginas, aprimorando habilidades em responsividade e semântica."
+            }, 
+            {
+                titulo: "JavaScript",
+                descricao: "Fortaleci minhas habilidades de manipulação do DOM (Document Object Model) e lógica de programação."
+            }
+        ],
         tecnologias: ["HTML5", "CSS3", "JavaScript", "Flask", "MySQL"],
         github: "https://github.com/Draco-Imperium/API_FATEC1",
         youtube: "https://www.youtube.com/watch?v=NQjO5y1g220"
@@ -19,12 +45,34 @@ const projetos = [
         titulo: "Dashboard de Indicadores",
         semestre: "2° Semestre - 2025-1",
         imagem: "/img/spoiler-projetos/spoiler-api2.png",
-        problema: "A Helpnei precisava de um jeito melhor de mostrar para as empresas parceiras o impacto e os resultados obtidos dentro da plataforma. Até então, esses dados eram pouco organizados e difíceis de visualizar.",
+        problema: "O projeto foi proposto pela Helpnei, que precisava de um jeito melhor de mostrar para as empresas parceiras o impacto e os resultados obtidos dentro da plataforma. Até então, esses dados eram pouco organizados e difíceis de visualizar.",
         solucao: "Desenvolvemos um dashboard de indicadores para monitorar o impacto da plataforma e acompanhar o crescimento das empresas cadastradas no Helpnei, exibindo dados estratégicos para patrocinadores e investidores. E um sistema de captação de usuários interessados em patrocínio.",
         contribuicao: "Fui uma das responsáveis por montar a estrutura das páginas em HTML e fazer a parte visual com CSS. Também desenvolvi um mapa interativo, que mostra a distribuição geográfica dos usuários.",
         papel: "Dev Team",
-        softSkills: ["Colaboração", "Resolução de problemas", "Organização e planejamento"],
-        hardSkills: ["HTML e CSS", "Noções de UX design", "Visualização de dados"],
+        softSkills: [
+            {
+                titulo: "Colaboração",
+                descricao: "Trabalhei em conjunto com o time para manter alinhamento visual e funcional entre todas as telas."
+            }, 
+            {
+                titulo: "Resolução de problemas",
+                descricao: "Enfrentei desafios técnicos no mapa interativo e busquei soluções eficientes até chegar no resultado ideal."
+            }, 
+            {
+                titulo: "Organização e planejamento",
+                descricao: "Estruturei minhas tarefas e acompanhei o cronograma do time, mantendo o fluxo de trabalho claro e organizado."
+            }
+        ],
+        hardSkills: [
+            {
+                titulo: "HTML e CSS",
+                descricao: "Fui responsável pela estruturação das páginas do dashboard aprimorando minha capacidade de criar layouts limpos, responsivos e padronizados."
+            }, 
+            {
+                titulo: "Noções de UX design",
+                descricao: "Aprimorei minha habilidade de organizar informações pensando na experiência do usuário, com foco em clareza e facilidade de navegação."
+            }
+        ],
         tecnologias: ["HTML5", "CSS3", "JavaScript", "React", "Sass", "TypeScript", "NodeJS"],
         github: "https://github.com/GeneSys-fatec/API-2DSM",
         youtube: "https://www.youtube.com/watch?v=0kPp-YthV4Y"
@@ -34,12 +82,46 @@ const projetos = [
         titulo: "Gerenciamento de Tarefas (To-do)",
         semestre: "3° Semestre - 2025-2",
         imagem: "/img/spoiler-projetos/spoiler-api3.png",
-        problema: "A GSW enfrentava problemas no controle das tarefas, comunicação falha, atividades que se perdiam no meio do caminho e atrasos nas entregas. Faltava uma ferramenta simples que centralizasse tudo.",
+        problema: "O projeto foi proposto pela GSW, que enfrentava problemas no controle das tarefas, comunicação falha, atividades que se perdiam no meio do caminho e atrasos nas entregas. Faltava uma ferramenta simples que centralizasse tudo.",
         solucao: "Criamos um gerenciador de tarefas que ajuda as equipes a se organizarem de forma prática. A ideia é facilitar o acompanhamento das atividades, melhorar a comunicação e evitar esquecimentos no dia a dia.",
         contribuicao: "Fiquei responsável por cuidar da documentação (DoR, DoD e Manual do Usuário), dos wireframes e da distribuição das tarefas entre os integrantes. Também participei minimamente como desenvolvedora, ajudando em ajustes visuais, melhorias na responsividade e na lógica da seção de comentários.",
         papel: "Product Owner",
-        softSkills: ["Comunicação clara", "Gestão de tempo", "Empatia e escuta ativa", "Adaptabilidade"],
-        hardSkills: ["Product Ownership", "Criação de wireframes", "Gestão de backlog", "Metodologias Ágeis"],
+        softSkills: [
+            {
+                titulo: "Comunicação clara",
+                descricao: "Melhorei a forma de transmitir requisitos e alinhar expectativas com o time e com o cliente."
+            }, 
+            {
+                titulo: "Gestão de tempo",
+                descricao: "Equilibrei atividades de gestão com pequenas contribuições no frontend, organizando bem a execução das tarefas."
+            }, 
+            {
+                titulo: "Empatia e escuta ativa",
+                descricao: "Aprendi a considerar tanto as necessidades do cliente quanto as limitações e sugestões do time."
+            }, 
+            {
+                titulo: "Adaptabilidade",
+                descricao: "Criei os documentos de acordo com as necessidades do cliente, fazendo ajustes quando necessário."
+            }
+        ],
+        hardSkills: [
+            {
+                titulo: "Product Ownership",
+                descricao: "Atuei como PO, transformando necessidades da empresa parceira em histórias de usuário claras e priorizando o que era essencial para eles."
+            }, 
+            {
+                titulo: "Criação de wireframes",
+                descricao: "Desenvolvi protótipos que facilitaram o entendimento das funcionalidades e do fluxo do sistema."
+            }, 
+            {
+                titulo: "Gestão de backlog",
+                descricao: "Organizei e mantive o backlog atualizado, além de produzir artefatos como DoR, DoD e o Manual do Usuário."
+            }, 
+            {
+                titulo: "Metodologias Ágeis",
+                descricao: "Participei ativamente das cerimônias da sprint, aprendendo a adaptar demandas e priorizações conforme o andamento do projeto."
+            }
+        ],
         tecnologias: ["HTML5", "CSS3", "JavaScript", "React", "TailwindCSS", "TypeScript", "Java", "Sprint Boot", "MongoDB", "AWS"],
         github: "https://github.com/GeneSys-fatec/API-3DSM",
         youtube: ""
@@ -50,7 +132,24 @@ const projetos = [
         imagem: "/img/spoiler-projetos/spoiler-chatbot.png",
         solucao: "Desenvolvimento de um chatbot para Telegram para ajudar usuários a encontrar livros de forma rápida e fácil. É possível pesquisar por título, autor ou gênero, receber recomendações personalizadas e descobrir novos títulos de interesse em pouquíssimo tempo.",
         contribuicao: "Atuei na personalização do modelo de linguagem e na integração do chatbot com o Telegram, garantindo que as respostas fossem adequadas ao contexto e que o funcionamento dentro da plataforma fosse fluido.",
-        hardSkills: ["LLMs", "Ollama", "IA", "Google Colab"],
+        hardSkills: [
+            {
+                titulo: "LLMs",
+                descricao: "Ajustei o comportamento do modelo para melhorar a precisão e relevância das respostas, entendendo melhor como trabalhar com IA generativa."
+            }, 
+            {
+                titulo: "Ollama",
+                descricao: "Aprendi a configurar e testar modelos localmente, explorando parâmetros e otimizações de desempenho."
+            }, 
+            {
+                titulo: "IA",
+                descricao: "Implementei a integração do bot com a plataforma, garantindo que as respostas fossem enviadas e recebidas corretamente."
+            }, 
+            {
+                titulo: "Google Colab",
+                descricao: "Utilizei o Colab para testar e rodar a aplicação."
+            }
+        ],
         tecnologias: ["Python", "Ollama", "SQLite", "Fast MCP", "Fast API"],
         github: "https://github.com/anajgaspar/chatbot-IHC",
         youtube: ""
